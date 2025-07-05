df859dec9ff089e8e12906f6079019ecfa7ddcb0
Taha Mohammed Naser <71102643+Tahactw@users.noreply.github.com>
2025-07-05 21:19:50 +0300

    Delete src directory

--- src/components/Contact.tsx
+++
@@ -1,9 +0,0 @@
-const Contact = () => (
-  <section className="mb-10" id="contact">
-    <h2 className="text-2xl font-semibold mb-4">Contact</h2>
-    <p>
-      Feel free to reach out via email: <a href="mailto:taha@example.com" className="text-blue-600 underline">taha@example.com</a>
-    </p>
-  </section>
-)
-export default Contact
